2362071ea36611a1b16bf7b7f87d85ec9d327e8f
Gokhil333 <141570134+Gokhil333@users.noreply.github.com>
2023-08-06 20:32:56 +0530

    Add files via upload

---
+++ Utube_data_sql_streamlit.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[82]:
+
+
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import mysql.connector as sql
+
+
+# In[83]:
+
+
+mydb = sql.connect(host="localhost",
+                   user="root",
+                   password="9500",
+                   database= "gokhil_ds_mysql"
+                  )
+mycursor = mydb.cursor()
+mycursor
+
+
+# In[84]:
+
+
+st.set_page_config(page_title= "Youtube Data harvesting and warehousing | By Gokhil S",
+                   layout= "wide",
+                   initial_sidebar_state= "expanded",
+                   menu_items={'About': """# This app is created by *Gokhil S!*"""})
+with st.sidebar:
+    selected = option_menu(None, ["Home","Extract and Transform","View"], 
+                           icons=["house-door-fill","tools","card-text"],
+                           default_index=0,
+                           orientation="vertical",
+                           styles={"nav-link": {"font-size": "30px", "text-align": "centre", "margin": "0px", 
+                                                "--hover-color": "#131314"},
+                                   "icon": {"font-size": "30px"},
+                                   "container" : {"max-width": "6000px"},
+                                   "nav-link-selected": {"background-color": "#1b1c70"}})
+
+
+# In[85]:
+
+
+if selected == "Home":
+    col1,col2 = st.columns(2,gap= 'medium')
+    col1.markdown("## :blue[Domain] : Social Media")
+    col1.markdown("## :blue[Technologies used] : Python,MongoDB, Youtube Data API, MySql, Streamlit")
+    col1.markdown("## :blue[Overview] : Retrieving the Youtube channels data from the Google API, storing it in a MongoDB as data lake, migrating and transforming data into a SQL database,then querying the data and displaying it in the Streamlit app.")
+    col2.markdown("#   ")
+    col2.markdown("#   ")
+    col2.markdown("#   ")
+
+
+# In[86]:
+
+
+if selected == "Extract and Transform":
+    tab1,tab2 = st.tabs(["$\huge EXTRACT $", "$\huge TRANSFORM $"])
+    with tab1:
+        st.markdown("#    ")
+        st.write("### Enter YouTube Channel_ID below :")
+        ch_id = st.text_input("Hint : Goto channel's home page > Right click > View page source > Find channel_id").split(',')
+        if ch_id and st.button("Extract Data"):
+            ch_details = get_channel_details(ch_id)
+            st.write(f'#### Extracted data from :green["{ch_details[0]["Channel_name"]}"] channel')
+            st.table(ch_details)
+        if st.button("Upload to MongoDB"):
+            with st.spinner('Please Wait for it...'):
+                ch_details = get_channel_details(ch_id)
+                v_ids = get_channel_videos(ch_id)
+                vid_details = get_video_details(v_ids)
+                def comments():
+                    com_d = []
+                    for i in v_ids:
+                        com_d+= get_comments_details(i)
+                    return com_d
+                comm_details = comments()
+
+                collections1 = db.channel_details
+                collections1.insert_many(ch_details)
+
+                collections2 = db.video_details
+                collections2.insert_many(vid_details)
+
+                collections3 = db.comments_details
+                collections3.insert_many(comm_details)
+                st.success("Upload to MogoDB successful !!")
+                
+    with tab2:     
+        st.markdown("#   ")
+        st.markdown("### Select a channel to begin Transformation to SQL")
+        
+        ch_names = channel_names()
+        user_inp = st.selectbox("Select channel",options= ch_names)
+        
+        def insert_into_channels():
+                collections = db.channel_details
+                query = """INSERT INTO channels VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"""
+                
+                for i in collections.find({"Channel_name" : user_inp},{'_id':0}):
+                    mycursor.execute(query,tuple(i.values()))
+                    mydb.commit()
+                
+        def insert_into_videos():
+            collectionss = db.video_details
+            query1 = """INSERT INTO videos VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+            for i in collectionss.find({"Channel_name" : user_inp},{"_id":0}):
+                t=tuple(i.values())
+                mycursor.execute(query1,t)
+                mydb.commit()
+
+        def insert_into_comments():
+            collections1 = db.video_details
+            collections2 = db.comments_details
+            query2 = """INSERT INTO comments VALUES(%s,%s,%s,%s,%s,%s,%s)"""
+
+            for vid in collections1.find({"Channel_name" : user_inp},{'_id' : 0}):
+                for i in collections2.find({'Video_id': vid['Video_id']},{'_id' : 0}):
+                    t=tuple(i.values())
+                    mycursor.execute(query2,t)
+                    mydb.commit()
+
+        if st.button("Submit"):
+            try:
+                
+                insert_into_channels()
+                insert_into_videos()
+                insert_into_comments()
+                st.success("Transformation to MySQL Successful!!!")
+            except:
+                st.error("Channel details already transformed!!")
+
+
+# In[87]:
+
+
+if selected == "View":
+    
+    st.write("## :orange[Select any question to get Insights]")
+    questions = st.selectbox('Questions',
+    ['Click the question that you would like to query',
+    '1. What are the names of all the videos and their corresponding channels?',
+    '2. Which channels have the most number of videos, and how many videos do they have?',
+    '3. What are the top 10 most viewed videos and their respective channels?',
+    '4. How many comments were made on each video, and what are their corresponding video names?',
+    '5. Which videos have the highest number of likes, and what are their corresponding channel names?',
+    '6. What is the total number of likes and dislikes for each video, and what are their corresponding video names?',
+    '7. What is the total number of views for each channel, and what are their corresponding channel names?',
+    '8. What are the names of all the channels that have published videos in the year 2022?',
+    '9. What is the average duration of all videos in each channel, and what are their corresponding channel names?',
+    '10. Which videos have the highest number of comments, and what are their corresponding channel names?'])
+    
+    if questions == '1. What are the names of all the videos and their corresponding channels?':
+        mycursor.execute("""SELECT title AS Video_Title, channel_name AS Channel_Name FROM videos ORDER BY channel_name""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+        
+    elif questions == '2. Which channels have the most number of videos, and how many videos do they have?':
+        mycursor.execute("""SELECT channel_name 
+        AS Channel_Name, total_videos AS Total_Videos
+                            FROM channels
+                            ORDER BY total_videos DESC""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+        st.write("### :green[Number of videos in each channel :]")
+        ig = px.bar(df,
+                     x=mycursor.column_names[0],
+                     y=mycursor.column_names[1],
+                     orientation='v',
+                     color=mycursor.column_names[0]
+                    )
+        st.plotly_chart(fig,use_container_width=True)
+        
+    elif questions == '3. What are the top 10 most viewed videos and their respective channels?':
+        mycursor.execute("""SELECT channel_name AS Channel_Name, title AS Video_Title, views AS Views 
+                            FROM videos
+                            ORDER BY views DESC
+                            LIMIT 10""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+        st.write("### :green[Top 10 most viewed videos :]")
+        fig = px.bar(df,
+                     x=mycursor.column_names[2],
+                     y=mycursor.column_names[1],
+                     orientation='h',
+                     color=mycursor.column_names[0]
+                    )
+        st.plotly_chart(fig,use_container_width=True)
+        
+    elif questions == '4. How many comments were made on each video, and what are their corresponding video names?':
+        mycursor.execute("""SELECT a.video_id AS Video_id, a.title AS Video_Title, b.Total_Comments
+                            FROM videos AS a
+                            LEFT JOIN (SELECT video_id,COUNT(comment_id) AS Total_Comments
+                            FROM comments GROUP BY video_id) AS b
+                            ON a.video_id = b.video_id
+                            ORDER BY b.Total_Comments DESC""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+          
+    elif questions == '5. Which videos have the highest number of likes, and what are their corresponding channel names?':
+        mycursor.execute("""SELECT channel_name AS Channel_Name,title AS Title,likes AS Likes_Count 
+                            FROM videos
+                            ORDER BY likes DESC
+                            LIMIT 10""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+        st.write("### :green[Top 10 most liked videos :]")
+        fig = px.bar(df,
+                     x=mycursor.column_names[2],
+                     y=mycursor.column_names[1],
+                     orientation='h',
+                     color=mycursor.column_names[0]
+                    )
+        st.plotly_chart(fig,use_container_width=True)
+        
+    elif questions == '6. What is the total number of likes and dislikes for each video, and what are their corresponding video names?':
+        mycursor.execute("""SELECT title AS Title, likes AS Likes_Count
+                            FROM videos
+                            ORDER BY likes DESC""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+         
+    elif questions == '7. What is the total number of views for each channel, and what are their corresponding channel names?':
+        mycursor.execute("""SELECT channel_name AS Channel_Name, views AS Views
+                            FROM channels
+                            ORDER BY views DESC""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+        st.write("### :green[Channels vs Views :]")
+        fig = px.bar(df,
+                     x=mycursor.column_names[0],
+                     y=mycursor.column_names[1],
+                     orientation='v',
+                     color=mycursor.column_names[0]
+                    )
+        st.plotly_chart(fig,use_container_width=True)
+        
+    elif questions == '8. What are the names of all the channels that have published videos in the year 2022?':
+        mycursor.execute("""SELECT channel_name AS Channel_Name
+                            FROM videos
+                            WHERE published_date LIKE '2022%'
+                            GROUP BY channel_name
+                            ORDER BY channel_name""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+        
+    elif questions == '9. What is the average duration of all videos in each channel, and what are their corresponding channel names?':
+        mycursor.execute("""SELECT channel_name, 
+                        SUM(duration_sec) / COUNT(*) AS average_duration
+                        FROM (
+                            SELECT channel_name, 
+                            CASE
+                                WHEN duration REGEXP '^PT[0-9]+H[0-9]+M[0-9]+S$' THEN 
+                                TIME_TO_SEC(CONCAT(
+                                SUBSTRING_INDEX(SUBSTRING_INDEX(duration, 'H', 1), 'T', -1), ':',
+                            SUBSTRING_INDEX(SUBSTRING_INDEX(duration, 'M', 1), 'H', -1), ':',
+                            SUBSTRING_INDEX(SUBSTRING_INDEX(duration, 'S', 1), 'M', -1)
+                            ))
+                                WHEN duration REGEXP '^PT[0-9]+M[0-9]+S$' THEN 
+                                TIME_TO_SEC(CONCAT(
+                                '0:', SUBSTRING_INDEX(SUBSTRING_INDEX(duration, 'M', 1), 'T', -1), ':',
+                                SUBSTRING_INDEX(SUBSTRING_INDEX(duration, 'S', 1), 'M', -1)
+                            ))
+                                WHEN duration REGEXP '^PT[0-9]+S$' THEN 
+                                TIME_TO_SEC(CONCAT('0:0:', SUBSTRING_INDEX(SUBSTRING_INDEX(duration, 'S', 1), 'T', -1)))
+                                END AS duration_sec
+                        FROM videos
+                        ) AS subquery
+                        GROUP BY channel_name""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names
+                          )
+        st.write(df)
+        st.write("### :green[Average video duration for channels :]")
+        
+
+        
+    elif questions == '10. Which videos have the highest number of comments, and what are their corresponding channel names?':
+        mycursor.execute("""SELECT channel_name AS Channel_Name,video_id AS Video_ID,comments AS Comments
+                            FROM videos
+                            ORDER BY comments DESC
+                            LIMIT 10""")
+        df = pd.DataFrame(mycursor.fetchall(),columns=mycursor.column_names)
+        st.write(df)
+        st.write("### :green[Videos with most comments :]")
+        fig = px.bar(df,
+                     x=mycursor.column_names[1],
+                     y=mycursor.column_names[2],
+                     orientation='v',
+                     color=mycursor.column_names[0]
+                    )
+        st.plotly_chart(fig,use_container_width=True)
+
+
+# In[88]:
+
+
+def get_channel_data():
+    query = "SELECT * FROM channels"
+    df = pd.read_sql(query, engine)
+    return df
+
+
+# In[89]:
+
+
+def get_video_data():
+    query = "SELECT * FROM videos"
+    df = pd.read_sql(query, engine)
+    return df
+
+
+# In[90]:
+
+
+def get_comment_data():
+    query = "SELECT * FROM comments"
+    df = pd.read_sql(query, engine)
+    return df
+
+
+# In[ ]:
+
+
+
+
